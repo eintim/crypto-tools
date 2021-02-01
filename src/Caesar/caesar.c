645df576b1b17c7f42adbeff7a43debf88123fab
DrShadowPLay <52300261+DrShadowPLay@users.noreply.github.com>
2021-02-01 17:54:26 +0100

    Rechtschreibfehler

---
+++ src/Caesar/caesar.c
@@ -0,0 +1,9 @@
+#ifndef CAESAR
+#define CAESAR 1
+#include <stdio.h>
+
+int caesar_gui() {
+	 printf("hallo");
+}
+//int caesar_encryption(char* plain, int  )
+#endif // !CAESAR
--- src/ceasar/ceasar.c
+++
@@ -1,8 +0,0 @@
-#ifndef CAESER
-#define CAESER 1
-
-int caeser_gui() {
-	return 0;
-}
-//int ceaser_encryption(char* plain, int  )
-#endif // !CAESER
