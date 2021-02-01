d2094a2bce820189fcfc179a2ce20da103e744f3
DrShadowPLay <52300261+DrShadowPLay@users.noreply.github.com>
2021-02-01 18:59:16 +0100

    header fixed

--- src/Caesar/caesar.c
+++ src/Caesar/caesar.c
@@ -1,9 +1,7 @@
-#ifndef CAESAR
-#define CAESAR 1
+
 #include <stdio.h>
 
-int caesar_gui() {
-	 printf("hallo");
+void caesar_gui() {
+    printf("hallo");
 }
 //int caesar_encryption(char* plain, int  )
-#endif // !CAESAR
---
+++ src/Caesar/caesar.h
@@ -0,0 +1,4 @@
+#ifndef CAESAR
+#define CAESAR 0
+void caesar_gui();
+#endif // !CAESAR
