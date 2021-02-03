bb935d8ef588e8ddf607670bd36ba57b763fad6e
jeppard <35837126+jeppard@users.noreply.github.com>
2021-02-03 23:55:43 +0100

    added readFileInString
    function reads an entire file as chars in a string

---
+++ src/lib/readFileInString.c
@@ -0,0 +1,24 @@
+//
+// Created by Jonas on 03.02.2021.
+//
+
+#include "readFileInString.h"
+char * readFileInString(char * filepath, unsigned int maxLen){//Reads the file and returns read string
+    FILE * file;
+    file = fopen(filepath, "r");
+    if (file==NULL){
+        printf("%s\n"strerror(errno));
+        return NULL
+    }
+    char * str;
+    str = malloc(sizeof(char));
+    str[0] = '\0';
+    char tmp;
+    for (int i = 0; i < maxLen && ((tmp = fgetc(file)) != EOF); ++i) {
+        str = realloc(str, (i+2)* sizeof(char));
+        str[i] = tmp;
+        str[i+1]= '\0';
+    }
+    fclose(file);
+    return str
+}
---
+++ src/lib/readFileInString.h
@@ -0,0 +1,11 @@
+//
+// Created by Jonas on 03.02.2021.
+//
+
+#ifndef SRC_READFILEINSTRING_H
+#define SRC_READFILEINSTRING_H
+#include <stdio.h>
+#include <errno.h>
+#include <malloc.h>
+char * readFileInString(char * filepath, unsigned int maxLen);
+#endif //SRC_READFILEINSTRING_H
