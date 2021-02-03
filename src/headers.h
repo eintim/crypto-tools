ba2625ea0ae0f6a34caf2c915304033bea862a0c
jeppard <35837126+jeppard@users.noreply.github.com>
2021-02-03 13:13:50 +0100

    Fixed clearInputBuffer
    Fixed clearInputBuffer and deleted enigma from main

--- src/headers.h
+++ src/headers.h
@@ -4,6 +4,7 @@
 
 #ifndef SRC_HEADERS_H
 #define SRC_HEADERS_H 1
-#include "ceasar/ceasar.h"
 #include "lib/readString.h"
+#include "lib/clearInputBuffer.h"
+#include <stdio.h>
 #endif //SRC_HEADERS_H
