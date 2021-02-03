394541df0bb32979863a7d6702c0b0a0f7d38f62
jeppard <35837126+jeppard@users.noreply.github.com>
2021-02-04 00:04:14 +0100

    Menu + read plaintext in encrypt
    Menu for encrypt and able to read in plaintext

--- src/enigma/enigma.c
+++ src/enigma/enigma.c
@@ -3,21 +3,20 @@
 //
 
 #include "enigma.h"
-#include <stdio.h>
-#include <ctype.h>
-#include "../lib/clearInputBuffer.h"
+
 
 void enigma_gui(){
     char c;
-    printf("Enigma\n");
+    printf("Enigma Menu\n");
     do {
-        c = tolower(getchar());
+        printf("-> ");
+        c = tolower(readOneChar(stdin));
         switch (c) {
             case 'd':
                 //Todo Decrypt
                 break;
             case 'e':
-                //TODO Encrypt
+                enigma_encrypt_gui();
                 break;
             case 'q':
                 printf("Leaving Enigma...\n");
@@ -33,7 +32,6 @@ void enigma_gui(){
                 printf("Enter Valid char (h/H) for Help!\n");
                 break;
         }
-        clearInputBuffer(stdin);
     }while(c!='q');
 }
 
@@ -41,7 +39,34 @@ void enigma_encrypt_gui(){
     char c;
     printf("Enigma Encrypt\n");
     printf("Read from file?(y,N) ");
-    c = getchar();
+    c = tolower(readOneChar(stdin));
+    char * input_str;
+    if (tolower(c) == 'y'){ //Read input string from file
+        printf("Enter filename: ");
+        char * filename;
+        filename = readString(stdin, -1);
+        FILE* my_file = fopen(filename, "r");
+        if (my_file == NULL){
+            printf(strerror(errno));
+        }
+        input_str = malloc(sizeof (char));
+        input_str[0] = '\0';
+        char tmp;
+        unsigned int len = 0;
+        while ((tmp = getc(my_file) )!= EOF){
+            input_str = realloc(input_str, (len+2)*sizeof(char));
+            input_str[len] = tmp;
+            input_str[len+1] = '\0';
+            len++;
+        }
+        fclose(my_file);
+        free(filename);
+    }
+    else{
+        printf("Enter Text to encrypt:\n");
+        input_str = readString(stdin, -1);
+    }
 
-
+    //TODO
+    free(input_str);
 }
--- src/enigma/enigma.h
+++ src/enigma/enigma.h
@@ -5,6 +5,12 @@
 #ifndef SRC_ENIGMA_H
 #define SRC_ENIGMA_H
 #include "../lib/clearInputBuffer.h"
+#include "../lib/readString.h"
+#include "../lib/readOneChar.h"
+#include <malloc.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
 void enigma_gui();
 void enigma_encrypt_gui();
 void enigma_decrypt_gui();
