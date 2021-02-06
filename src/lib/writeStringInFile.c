7623bfb7fa5b9899a8723c274fca4e692356bf78
jeppard <35837126+jeppard@users.noreply.github.com>
2021-02-06 21:36:20 +0100

    added semicolon
    missed it

--- src/lib/writeStringInFile.c
+++ src/lib/writeStringInFile.c
@@ -8,7 +8,7 @@ int writeStringInFile(char * string, char * filename){
     FILE * file = fopen(filename, "w");
     if (file == NULL){
         printf("Error accured while writing file '%s': %s", filename, strerror(errno));
-        return errno
+        return errno;
     }
     else{
         for (int i = 0; string[i] != '\0' ; ++i) {
