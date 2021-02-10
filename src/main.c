123909b6c1d7dd59f7993c07c2fd5090e534e40b
eintim <15858437+eintim@users.noreply.github.com>
2021-02-10 11:01:20 +0100

    added basic main menu
    implemented basic main menu

--- src/main.c
+++ src/main.c
@@ -1,5 +1,36 @@
 #include "headers.h"
+
 int main(){
-    writeStringInFile("Hello World!", "Testfile.txt");
+    char c;
+    printf("Cryto Tools Menu\n");
+    do {
+        printf("-> ");
+        c = tolower(readOneChar(stdin));
+        switch (c) {
+            case 'e':
+                enigma_gui();
+                break;
+            case 'v':
+                vigenere_gui();
+                break;
+            case 'c':
+                //TODO caeser_gui();
+                break;
+            case 'q':
+                printf("Leaving Crypto Tools...\n");
+                break;
+            case 'h':
+                printf("Help:\n");
+                printf("\t [c]aesar \t- use caesar\n");
+                printf("\t [e]nigma \t- use enigma\n");
+                printf("\t [v]igenere \t- use vigenere\n");
+                printf("\t [h]elp \t- show this menu\n");
+                printf("\t [q]uit \t- leave this application\n");
+                break;
+            default:
+                printf("Enter Valid char (h/H) for Help!\n");
+                break;
+        }
+    }while(c!='q');
     return 0;
 }
